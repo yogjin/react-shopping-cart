7e3a86d359f50d7529a835a9bb8233566e1644f7
yogjin <youngjin988@gmail.com>
2023-05-28 13:09:37 +0900

    feat: 배포 시 msw 이용 가능하도록 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import store from 'utils/localStorage';
 
 async function main() {
-  if (process.env.NODE_ENV === 'development') {
-    if (window.location.pathname === '/react-shopping-cart') {
-      window.location.pathname = '/react-shopping-cart/';
-      return;
-    }
-
-    const { worker } = require('./mocks/browser');
-
-    await worker.start({
-      serviceWorker: {
-        url: '/react-shopping-cart/mockServiceWorker.js',
-      },
-    });
+  // if (process.env.NODE_ENV === 'development') {
+  if (window.location.pathname === '/react-shopping-cart') {
+    window.location.pathname = '/react-shopping-cart/';
+    return;
   }
+
+  const { worker } = require('./mocks/browser');
+
+  await worker.start({
+    serviceWorker: {
+      url: '/react-shopping-cart/mockServiceWorker.js',
+    },
+  });
+  // }
+
   const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
   root.render(
     <React.StrictMode>
